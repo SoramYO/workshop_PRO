6f6ecb7484fb061f0629027cc8ce218bd3d40b63
Ngo Xuan Son <90395601+SoramYO@users.noreply.github.com>
2023-06-26 15:24:00 +0700

    Add files via upload

---
+++ CompanyManagement/src/model/TeamLeader.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.ArrayList;
+
+public class TeamLeader extends Developer {
+
+    private double bonus_rate;
+
+    public TeamLeader(String empID, String empName, int baseBal, String teamName, ArrayList<String> programmingLanguages, int expYear, double bonus_rate) {
+        super(empID, empName, baseBal, teamName, programmingLanguages, expYear);
+        this.bonus_rate = bonus_rate;
+    }
+
+    public double getBonus_rate() {
+        return bonus_rate;
+    }
+
+    @Override
+    public double getSalary() {
+        return getSalary() + bonus_rate * getSalary();
+    }
+}
